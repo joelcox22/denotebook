3771f0b598fb5fee2355ce66a8b047f57b79fcda
Joel Cox <joel@pinecodes.com>
2024-11-07 20:13:51 +0800

    symbol docs for sankey

--- src/sankey.ts
+++ src/sankey.ts
@@ -1,6 +1,9 @@
 
 type SankeyEntry = [from: string, to: string, value: number];
 
+/**
+ * Display a sankey diagram.
+ */
 export function sankey(data: SankeyEntry[]): Deno.jupyter.Displayable {
   return Deno.jupyter.md`\`\`\`mermaid\nsankey-beta\n${data
     .map((row) => row.join(","))
