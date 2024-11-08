744cd34e56299ef1a2c15b410e758ee6df2adb97
Joel Cox <joel@pinecodes.com>
2024-11-08 18:40:43 +0800

    test

--- src/sankey.ts
+++ src/sankey.ts
@@ -9,3 +9,13 @@ export function sankey(data: SankeyEntry[]): Deno.jupyter.Displayable {
     .map((row) => row.join(","))
     .join("\n")}\n\`\`\``;
 }
+
+
+/**
+ * Display a sankey diagram.
+ */
+export function sankey2(data: SankeyEntry[]): Deno.jupyter.Displayable {
+  return Deno.jupyter.md`\`\`\`mermaid\nsankey-beta\n${data
+    .map((row) => row.join(","))
+    .join("\n")}\n\`\`\``;
+}
