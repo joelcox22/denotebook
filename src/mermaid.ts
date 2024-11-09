5be159ec516bfb2ee5cb76ec9a598aa165afd3f7
Joel Cox <joel@pinecodes.com>
2024-11-09 10:56:25 +0800

    test themes

--- src/mermaid.ts
+++ src/mermaid.ts
@@ -10,19 +10,30 @@ console.warn = () => {};
 const browser = await puppeteer.launch();
 console.warn = oldLog;
 
-let config: any = {};
+const defaultConfig: any = {
+  config: {
+    theme: 'base',
+  },
+};
+
+let config: any = {...defaultConfig};
 
 export async function mermaid(graph: string | TemplateStringsArray): Promise<void> {
   const mmd = `---\n${stringify(config)}---\n${graph.toString()}`;
+  console.log(mmd);
   const { data } = await renderMermaid(browser, mmd, 'svg', {
-    // backgroundColor: 'transparent',
-    mermaidConfig: {
-      // theme: 'dark',
-    },
+    backgroundColor: 'transparent',
+    myCSS: `
+      #my-svg .flowchartTitleText { fill: var(--jp-ui-inverse-font-color0); }
+      #my-svg .flowchart-link { stroke: var(--jp-ui-inverse-font-color0); }
+      #my-svg .arrowMarkerPath { fill: var(--jp-ui-font-color0); stroke: none; }
+    `,
   });
   Deno.jupyter.display(Deno.jupyter.svg`${Buffer.from(data).toString()}`);
 }
 
 mermaid.configure = function(newConfig: any): void {
-  config = newConfig;
+  Object.assign(config, newConfig);
 }
+
+mermaid.resetConfig = () => config = { ...defaultConfig };
